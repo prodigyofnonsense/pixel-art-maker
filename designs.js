6fada354f36b56b68eb1f5585010e95ae540be16
prodigyofnonsense <97120148+prodigyofnonsense@users.noreply.github.com>
2022-03-28 13:08:58 -0400

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,33 @@
+// Select color input
+// Select size input
+var canvas = document.getElementById("pixelCanvas");
+var color = document.getElementById("colorPicker");
+var sizePicker = document.getElementById("sizePicker");
+var height = document.getElementById("inputHeight");
+var width = document.getElementById("inputWidth");
+
+// When size is submitted by the user, call makeGrid()
+function makeGrid(height, width) {
+    for (let y = 0; y < height; y++) {
+        let row = canvas.insertRow(y);
+    for (let x = 0; x < width; x++) {
+        let cell = row.insertCell(x);
+            cell.addEventListener("mousedown", function(evt) {
+            cell.style.backgroundColor = color.value;
+            cell.addEventListener("contextmenu", function(evt) {
+            evt.preventDefault();
+            cell.style.backgroundColor = "white";
+            } )
+            } )
+        }
+    }
+}
+
+//function that uses the size picker submit to call makeGrid() function
+sizePicker.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    while (canvas.hasChildNodes()) {
+        canvas.removeChild(canvas.lastChild);
+    }
+    makeGrid(height.value, width.value);
+});
