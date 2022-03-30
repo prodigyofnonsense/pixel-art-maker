3d5df70b304491b7a0f955f178daf8542a057d10
prodigyofnonsense <97120148+prodigyofnonsense@users.noreply.github.com>
2022-03-30 19:34:39 -0400

    Add files via upload

--- designs.js
+++ designs.js
@@ -8,26 +8,28 @@ var width = document.getElementById("inputWidth");
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid(height, width) {
-    for (let y = 0; y < height; y++) {
-        let row = canvas.insertRow(y);
-    for (let x = 0; x < width; x++) {
-        let cell = row.insertCell(x);
-            cell.addEventListener("mousedown", function(evt) {
-            cell.style.backgroundColor = color.value;
-            cell.addEventListener("contextmenu", function(evt) {
-            evt.preventDefault();
-            cell.style.backgroundColor = "white";
-            } )
-            } )
-        }
-    }
+	for (let y = 0; y < height; y++) {
+		let row = canvas.insertRow(y);
+		for (let x = 0; x < width; x++) {
+			let cell = row.insertCell(x);
+
+// When color is submitted by the user
+			cell.addEventListener("mousedown", function(event) {
+				cell.style.backgroundColor = color.value;
+				cell.addEventListener("contextmenu", function(event) {
+					event.preventDefault();
+					cell.style.backgroundColor = "white";
+				})
+			})
+		}
+	}
 }
 
 //function that uses the size picker submit to call makeGrid() function
-sizePicker.addEventListener("submit", function(evt) {
-    evt.preventDefault();
-    while (canvas.hasChildNodes()) {
-        canvas.removeChild(canvas.lastChild);
-    }
-    makeGrid(height.value, width.value);
+sizePicker.addEventListener("submit", function(event) {
+	event.preventDefault();
+	while (canvas.hasChildNodes()) {
+		canvas.removeChild(canvas.lastChild);
+	}
+	makeGrid(height.value, width.value);
 });
